6716a2849665734e1ff407f134ef6a29b7317304
Philip Werz <pjwerz@gmx.de>
2021-04-24 16:49:33 +0200

    second commit

--- app/src/main/java/com/example/projectberta/MainActivity.java
+++ app/src/main/java/com/example/projectberta/MainActivity.java
@@ -7,16 +7,22 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView message;
-    private Button myButton;
-    private Button secondPage;
-    private int counter = 0;
+    private TextView randomMessage;
+    Random random = new Random();
+
+    String[] messages = {"stay safe! ", "good luck! ", "eat healthy! "};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        randomMessage = findViewById(R.id.randomMessage);
+        int randomInt = random.nextInt(3);
+        randomMessage.setText(messages[randomInt]);
 
         /*message = findViewById(R.id.viewCounter);
         myButton = findViewById(R.id.button);
